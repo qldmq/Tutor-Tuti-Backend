568bfa966064e4a434d5afd064bce09e8c4ec85b
qldmq <ksh13345@naver.com>
2025-02-27 15:10:26 +0900

    Feat | 공지글 좋아요 기능 구현

--- src/main/java/com/example/tutoring/repository/NoticeRepository.java
+++ src/main/java/com/example/tutoring/repository/NoticeRepository.java
@@ -1,6 +1,7 @@
 package com.example.tutoring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.example.tutoring.entity.Notice;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Integer> {
@@ -18,4 +20,11 @@ public interface NoticeRepository extends JpaRepository<Notice, Integer> {
     // 게시글 카운트
 	@Query("SELECT COUNT(n) FROM Notice n WHERE n.memberNum = :memberNum")
   	int noticeCount(@Param("memberNum") int memberNum);
+
+    @Query("SELECT n.likeCnt FROM Notice n WHERE n.noticeNum = :noticeNum")
+    Integer findLikeCntByNoticeNum(int noticeNum);
+
+    @Modifying
+    @Query("UPDATE Notice n SET n.likeCnt = :likeCnt WHERE n.noticeNum = :noticeNum")
+    void updateLikeCount(@Param("noticeNum") int noticeNum, @Param("likeCnt") int likeCnt);
 }
